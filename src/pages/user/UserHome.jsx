0846887e536c3d082217008bed084f1d31546f88
hwy-23db <hwyyy22@gmail.com>
2026-01-20 20:55:08 +0630

    fix qr scan in mobile

--- src/pages/user/UserHome.jsx
+++ src/pages/user/UserHome.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getBlogs } from "../../api/UserApi";
+import { getBlogs } from "../../api/userApi";
 
 function normalizeList(payload) {
   if (Array.isArray(payload)) return payload;
